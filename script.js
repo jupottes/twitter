ec860981c30ce1eb1cbbeef2c72006f812e4d189
jupottes <juliettepottes@gmail.com>
2019-02-21 10:25:51 -0300

    versão 1

---
+++ script.js
@@ -0,0 +1,9 @@
+var tweetText = document.getElementById("tweetText");
+var buttonText = document.getElementById("button");
+var feedPost = documento.getElementById("feed-post");
+
+buttonText.addEventListener("click", post)
+
+function post(){
+  document.getElementById('feed-post').innerText = valor
+}
